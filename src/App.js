b14ded47bee9af2e22a011bff6d34e9a1aa8ba1a
johnstonvs <johnstonvs@gmail.com>
2023-06-06 23:14:37 -0500

    Completes more practical fetch, realization that you cannot retrieve data within a month old.

--- src/App.js
+++ src/App.js
@@ -2,67 +2,79 @@ import { useState, useEffect } from 'react';
 
 function App() {
 
-  const [data, setData] = useState([]);
+  const [todaysTransactions, setTodaysTransactions] = useState();
   const [trades, setTrades] = useState([]);
   let today = new Date();
-  let year = '2023'//today.getFullYear();
-  let day = '10'//String(today.getDate()).padStart(2, '0');
-  let month = '03'//String(today.getMonth() + 1).padStart(2, '0');
+  let year = today.getFullYear().toString();
+  let day = String(today.getDate()).padStart(2, '0');
+  let month = String(today.getMonth() + 1).padStart(2, '0');
 
   useEffect(() => {
-
-    fetch('https://house-stock-watcher-data.s3-us-west-2.amazonaws.com/data/filemap.xml')
-      .then((response) => response.text())
-      .then(async (response) => {
-        const parser = new DOMParser()
-        const xml = parser.parseFromString(response, 'text/xml')
-        const results = [].slice.call(xml.getElementsByTagName('Key')).filter((key) => key.textContent.includes('.json'))
-        let files = results.map(file => file.textContent.split('/')[1])
-        files = await files.slice(0, 50).filter(item => {
-          // console.log(item.slice(23, 25), month, item.slice(26, 28), day, item.slice(29, 33), year.toString())
-          return item.slice(26, 28) === day && item.slice(23, 25) === month && item.slice(29, 33) === year.toString();
-        })
-        setData(files);
-        console.log(data);
+    fetch(`https://house-stock-watcher-data.s3-us-west-2.amazonaws.com/data/transaction_report_for_05_${day}_${year}.json`)
+      .then(res => res.json())
+      .then(data => {
+        setTodaysTransactions(data);
+        console.log(todaysTransactions);
       })
-      .catch((err) => {
-        console.log(err)
+      .catch(err => {
+        console.log(`catch: ${err}`)
       })
   }, [])
 
-  useEffect(() => {
-    const newTrades = [];
-    const promises = [];
-    data.forEach(path => {
-      promises.push(
-        fetch(`https://house-stock-watcher-data.s3-us-west-2.amazonaws.com/data/${path}`)
-          .then(res => res.json())
-          .then(trade => {
-            console.log(trade);
-            newTrades.push(trade);
-          })
-          .catch(err => {
-            console.log(`Error getting trade at ${path}: ${err}`)
-          })
-      )
-    })
+  // useEffect(() => {
 
-    Promise.all(promises)
-      .then(() => {
-        const transactions = [];
-        newTrades.forEach(trade => {
-          //console.log(trade);
-          trade.forEach(innerTrade => {
-            innerTrade.transactions.forEach(transaction => {
-              transactions.push(transaction);
-              console.log('transactions', transactions);
-            })
-          })
-        })
-        console.log('trades', trades[0].transactions[0].amount);
-      })
+  //   fetch('https://house-stock-watcher-data.s3-us-west-2.amazonaws.com/data/filemap.xml')
+  //     .then((response) => response.text())
+  //     .then(async (response) => {
+  //       const parser = new DOMParser()
+  //       const xml = parser.parseFromString(response, 'text/xml')
+  //       const results = [].slice.call(xml.getElementsByTagName('Key')).filter((key) => key.textContent.includes('.json'))
+  //       let files = results.map(file => file.textContent.split('/')[1])
+  //       files = await files.slice(0, 50).filter(item => {
+  //         // console.log(item.slice(23, 25), month, item.slice(26, 28), day, item.slice(29, 33), year.toString())
+  //         return item.slice(26, 28) === day && item.slice(23, 25) === month && item.slice(29, 33) === year.toString();
+  //       })
+  //       setData(files);
+  //       console.log(data);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err)
+  //     })
+  // }, [])
 
-  }, [])
+  // useEffect(() => {
+  //   const newTrades = [];
+  //   const promises = [];
+  //   data.forEach(path => {
+  //     promises.push(
+  //       fetch(`https://house-stock-watcher-data.s3-us-west-2.amazonaws.com/data/${path}`)
+  //         .then(res => res.json())
+  //         .then(trade => {
+  //           console.log(trade);
+  //           newTrades.push(trade);
+  //         })
+  //         .catch(err => {
+  //           console.log(`Error getting trade at ${path}: ${err}`)
+  //         })
+  //     )
+  //   })
+
+  //   Promise.all(promises)
+  //     .then(() => {
+  //       const transactions = [];
+  //       newTrades.forEach(trade => {
+  //         //console.log(trade);
+  //         trade.forEach(innerTrade => {
+  //           innerTrade.transactions.forEach(transaction => {
+  //             transactions.push(transaction);
+  //             console.log('transactions', transactions);
+  //           })
+  //         })
+  //       })
+  //       console.log('trades', trades[0].transactions[0].amount);
+  //     })
+
+  // }, [])
 
 
   return (
